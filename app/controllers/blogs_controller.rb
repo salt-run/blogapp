5c7cbc37fd112e52e3b1fd84b22b2f9e7d99614d
Hideto Shiozawa <shiorange10593@yahoo.co.jp>
2017-10-10 14:19:21 +0000

    ブログを作成する

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,51 @@
+class BlogsController < ApplicationController
+  
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @blogs = Blog.all
+  end
+
+  def show
+  end
+
+  def new
+    @blog = Blog.new
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを更新しました"
+    else
+      render 'edit'
+    end
+  end
+  
+  def create
+
+    @blog = Blog.create(blog_params)
+    if @blog.save
+      redirect_to blogs_path, notice: "ブログを作成しました！"
+    else
+      render "new"
+    end
+    
+  end
+  
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice: "ブログを削除しました"
+  end
+  
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+  
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+end
